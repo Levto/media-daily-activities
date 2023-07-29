75f448ea6aca557ae9f8fbdf2e4cf053c37477fe
Levto <43031097+Levto@users.noreply.github.com>
2023-07-29 22:56:34 +0700

    Lesson Contents Addition
    Added up to 3rd lesson contents

---
+++ display_scenes/lesson_content.gd
@@ -0,0 +1,56 @@
+extends Control
+
+@onready var dialogue = $dialogue_box/db_margin/db_vbox_container/dialogue
+@onready var malkuth = $malkuth
+@onready var content = $content
+@onready var next = $dialogue_box/dialogue_next
+@onready var prev = $dialogue_box/dialogue_prev
+
+var key = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	main_dictionaries.selected_lesson = "lesson_3"
+	dialogue.visible_characters = 0
+	$Timer.start()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var malkuth_file_name = main_dictionaries.lesson_vn_resource[main_dictionaries.selected_lesson][str(key)].malkuth
+	var content_file_name = main_dictionaries.lesson_vn_resource[main_dictionaries.selected_lesson][str(key)].content
+	
+	dialogue.text = main_dictionaries.lesson_vn_resource[main_dictionaries.selected_lesson][str(key)].text
+	malkuth.texture = load("res://resources/images/malkuth/" + malkuth_file_name + ".png")
+	if content_file_name != "":
+		content.texture = load("res://resources/images/lesson_contents/" + main_dictionaries.selected_lesson + "/" + content_file_name+ ".png")
+	
+	var dialogue_length = dialogue.get_total_character_count()
+	var lesson_length = main_dictionaries.lesson_vn_resource[main_dictionaries.selected_lesson].size()
+	
+	if key == 1:
+		prev.visible = false
+	else:
+		prev.visible = true
+	if key == lesson_length: # or dialogue.visible_characters < dialogue_length:
+		next.visible = false
+	else:
+		next.visible = true
+	
+func _on_dialogue_prev_pressed():
+	key -= 1
+	dialogue.visible_characters = 0
+	$Timer.start()
+
+func _on_dialogue_next_pressed():
+	key += 1
+	dialogue.visible_characters = 0
+	$Timer.start()
+
+func _on_timer_timeout():
+	var dialogue_length = dialogue.get_total_character_count()
+	
+	if dialogue.visible_characters < dialogue_length:
+		dialogue.visible_characters +=1
+	else:
+		$Timer.stop()
--- resources/main_dictionaries.gd
+++ resources/main_dictionaries.gd
@@ -289,6 +289,257 @@ var vocab_a = {
 	}
 }
 
+#lesson content data
+var selected_lesson
+
+var lesson_vn_resource = {
+	"lesson_1" = {
+		"1" = {
+			"text": "Halo! Perkenalkan namaku Ellie, dan selamat datang di [color=orange]Ellie's English Class[/color]. Di sini aku akan memandu teman-teman untuk belajar Bahasa Inggris bersama. Mari kita mulai pelajaran kita hari ini. [color=aquamarine](Tekan tombol panah di sebelah kanan)[/color]",
+			"malkuth": "3",
+			"content": "1"
+		},
+		"2" = {
+			"text": "Kali ini kita akan belajar cara merangkai kalimat [color=yellow]Simple Present Tense[/color].",
+			"malkuth": "1",
+			"content": "2"
+		},
+		"3" = {
+			"text": "Kalimat Simple Present Tense adalah jenis kalimat yang digunakan untuk menyatakan suatu [color=yellow]fakta masa kini, kebiasaan, dan kalimat perintah[/color].",
+			"malkuth": "1",
+			"content": "2"
+		},
+		"4" = {
+			"text": "Untuk merangkai kalimat Simple Present Tense, cukup mudah loh. Pola kalimat Simple Present Tense cukup mirip dengan Bahasa Indonesia.",
+			"malkuth": "3",
+			"content": "2"
+		},
+		"5" = {
+			"text": "Mari kita lihat contoh di samping ini.",
+			"malkuth": "2",
+			"content": "3"
+		},
+		"6" = {
+			"text": "[color=aqua]Aku[/color] [color=green]membeli[/color] [color=orange]lima apel[/color], dan dalam Bahasa inggris menjadi [color=aqua]I[/color] [color=green]buy[/color] [color=orange]five apples[/color].",
+			"malkuth": "1",
+			"content": "3"
+		},
+		"7" = {
+			"text": "Kedua kalimat ini memiliki pola kalimat yang sama yaitu [color=aqua]Subject (Pelaku)[/color] + [color=green]Verb (Kata Kerja)[/color] + [color=orange]Object (Objek/Benda)[/color].",
+			"malkuth": "1",
+			"content": "4"
+		},
+		"8" = {
+			"text": "Dari contoh ini, subject dalam kalimat adalah [color=aqua]I[/color], kemudian verb adalah [color=green]buy[/color], dan object adalah [color=orange]five apples[/color].",
+			"malkuth": "1",
+			"content": "5"
+		},
+		"9" = {
+			"text": "Dengan satu pola sederhana ini, [color=aqua]Subject (Pelaku)[/color] + [color=green]Verb (Kata Kerja)[/color] + [color=orange]Object (Objek/Benda)[/color], teman-teman bisa merangkai beragam kalimat seperti contoh di samping ini.",
+			"malkuth": "2",
+			"content": "6"
+		},
+		"10" = {
+			"text": "Bagaimana? Cukup mudah bukan? ",
+			"malkuth": "3",
+			"content": "6"
+		},
+		"11" = {
+			"text": "Perlu teman-teman perhatikan, ada juga kata kerja yang tidak memerlukan objek, seperti contohnya di samping ini.",
+			"malkuth": "2",
+			"content": "7"
+		},
+		"12" = {
+			"text": "Itu saja untuk pelajaran pertama kali ini. Ingat, untuk Simple Preset Tense, pola dasar kalimatnya adalah [color=aqua]Subject (Pelaku)[/color] + [color=green]Verb (Kata Kerja)[/color] + [color=orange]Object (Objek/Benda)[/color]. Sampai berjumpa kembali di kelas lainnya!",
+			"malkuth": "1",
+			"content": ""
+		}
+	},
+	"lesson_2" = {
+		"1" = {
+			"text": "Halo! Selamat datang kembali di Ellie's English Class. Ellie akan memandu teman-teman lagi dalam pelajaran kali ini.",
+			"malkuth": "3",
+			"content": "1"
+		},
+		"2" = {
+			"text": "Dalam pelajaran sebelumnya, kita telah mempelajari pola kalimat dasar simple present tense. Pada pelajaran kali ini, kita akan membahas membahas tentang [color=yellow]perubahan kata kerja[/color] dalam simple present tense.",
+			"malkuth": "1",
+			"content": "2"
+		},
+		"3" = {
+			"text": "Jika teman-teman perhatikan lebih teliti, di pelajaran sebelumnya ada satu contoh kata kerja yang berubah pada contoh kalimat yang ditampilkan.",
+			"malkuth": "2",
+			"content": "3"
+		},
+		"4" = {
+			"text": "Perubahan kata kerja terdapat pada contoh ini, He [color=lime]eats[/color] an apple.",
+			"malkuth": "1",
+			"content": "4"
+		},
+		"5" = {
+			"text": "Kata kerja yang ada di kalimat tersebut merupakan kata “eat” yang artinya makan. Namun, dalam penggunaan di kalimat tersebut, berubah menjadi “[color=lime]eat[/color][color=yellow]s[/color]”.",
+			"malkuth": "1",
+			"content": "5"
+		},
+		"6" = {
+			"text": "Dalam pelajaran kali ini, kita akan belajar tentang penyebab terjadi perubahan kata kerja seperti contoh tadi, dan bagaimana menerapkannya dalam menyusun kalimat.",
+			"malkuth": "1",
+			"content": "5"
+		},
+		"7" = {
+			"text": "Pertama-tama, kita mulai dari kata ganti subjek yang ada di dalam Bahasa Inggris.",
+			"malkuth": "1",
+			"content": "6"
+		},
+		"8" = {
+			"text": "Seperti yang teman-teman lihat, ada beberapa kata ganti subjek yaitu [color=orange]They, We, I, You,[/color] [color=lime]He, She,[/color] dan [color=lime]It[/color].",
+			"malkuth": "1",
+			"content": "6"
+		},
+		"9" = {
+			"text": "Dari ketujuh kata ganti tersebut, kita kelompokkan menjadi dua, yang pertama [color=orange]They, We, I, You[/color], dan yang kedua [color=lime]He, She, It[/color].",
+			"malkuth": "1",
+			"content": "7"
+		},
+		"10" = {
+			"text": "Agar mudah diingat, untuk kelompok [color=yellow]They, We, I, You[/color] bisa teman-teman ingat sebagai “[color=yellow]Dewi Ayu[/color]”, dan untuk kelompok ini, verb (kata kerja) [color=orange]tidak mengalami perubahan[/color] dalam simple present tense.",
+			"malkuth": "2",
+			"content": "8"
+		},
+		"11" = {
+			"text": "Dan untuk kelompok kedua, [color=lime]He, She,[/color] dan [color=lime]It[/color], verb [color=aqua]akan berubah[/color], yang secara umum [color=aqua]ditambahkan huruf “S”[/color].",
+			"malkuth": "1",
+			"content": "9"
+		},
+		"12" = {
+			"text": "Jika kita lihat pada contoh sebelumnya, di sini verb [color=lime]eat[/color] ditambahkan “S” menjadi [color=lime]eat[/color][color=yellow]s[/color] karena subject dalam kalimatnya menggunakan “[color=lime]He[/color]”.",
+			"malkuth": "1",
+			"content": "5"
+		},
+		"13" = {
+			"text": "Agar mudah diingat, subject [color=lime]He/She/It[/color] [color=aqua]suka minum es[/color], jadi jika teman-teman menggunakannya di dalam kalimat simple present tense, [color=aqua]tambahkan huruf “S”[/color].",
+			"malkuth": "2",
+			"content": "10"
+		},
+		"14" = {
+			"text": "Catatan tambahan, sebagian besar verb cukup ditambahkan huruf “S” saja. Namun, untuk verb dengan akhiran -h, -s, ditambahkan “-es”, dan verb dengan akhiran –y, huruf “y” tersebut berubah menjadi “i” dan kemudian ditambahkan “-es” juga seperti contoh berikut ini.",
+			"malkuth": "1",
+			"content": "11"
+		},
+		"15" = {
+			"text": "Semakin sering teman-teman membaca teks Bahasa Inggris, akan semakin mudah untuk teman-teman membedakan mana yang cukup ditambahkan “S”, “ES”, atau “IES”. ",
+			"malkuth": "2",
+			"content": "11"
+		},
+		"16" = {
+			"text": "Itu saja untuk pelajaran kali ini. Ingat, jika teman-teman menggunakan subject “[color=lime]He/She/It[/color]” di kalimat simple present tense, tambahkan “[color=aqua]S[/color]” pada verb di kalimat tersebut!",
+			"malkuth": "3",
+			"content": "11"
+		}
+	},
+	"lesson_3" = {
+		"1" = {
+			"text": "Selamat datang kembali di Ellie's English Class! Seperti biasanya, Ellie akan memandu teman-teman lagi dalam pelajaran kali ini.",
+			"malkuth": "3",
+			"content": "1"
+		},
+		"2" = {
+			"text": "Pada pelajaran sebelumnya kita telah mempelajari struktur sederhana kalimat simple present tense.",
+			"malkuth": "1",
+			"content": "2"
+		},
+		"3" = {
+			"text": "Akan tetapi kalimat yang tersusun masih terlihat kurang lengkap karena hanya menyampaikan objek.",
+			"malkuth": "1",
+			"content": "2"
+		},
+		"4" = {
+			"text": "Mari kita lihat contoh kalimat yang kita gunakan pada pelajaran sebelumnya, he eats an apple. Kalimat ini menggunakan pola Subject + Verb + Object dan sudah menjadi suatu kalimat yang utuh.",
+			"malkuth": "1",
+			"content": "3"
+		},
+		"5" = {
+			"text": "Akan tetapi, kalimat ini masih kurang menjelaskan keterangan seperti, dimana dia makan? Kapan dia makan? Bersama siapa dia makan? Berapa kali dia makan?",
+			"malkuth": "1",
+			"content": "3"
+		},
+		"6" = {
+			"text": "Di sini lah kita gunakan [color=yellow]adverb[/color] untuk menjelaskan kalimat lebih lanjut lagi.",
+			"malkuth": "1",
+			"content": "3"
+		},
+		"7" = {
+			"text": "Untuk tiga pelajaran ke depan, kita akan membahas tentang [color=yellow]Adverb[/color] atau kata keterangan. Kita akan mempelajari tiga jenis adverb terlebih dahulu, yaitu adverb of [color=aqua]time[/color] (waktu), [color=lime]place[/color] (tempat), dan [color=orange]frequency[/color] (frekuensi). Mari kita mulai pelajaran kali ini.",
+			"malkuth": "1",
+			"content": "4"
+		},
+		"8" = {
+			"text": "Adverb pertama yang akan kita bahas adalah [color=aqua]adverb of time[/color], atau keterangan waktu. Seperti namanya, kita menggunakan adverb ini untuk menjelaskan waktu kejadian.",
+			"malkuth": "1",
+			"content": "5"
+		},
+		"9" = {
+			"text": "Perlu teman-teman ingat, kalimat [color=yellow]simple present tense[/color] adalah kalimat untuk menyatakan [color=yellow]fakta masa kini, kebiasaan,[/color] dan [color=yellow]perintah[/color].",
+			"malkuth": "1",
+			"content": "5"
+		},
+		"10" = {
+			"text": "Oleh karena itu, hanya gunakan adverb of time yang menyatakan waktu masa kini atau waktu secara umum. [color=orange]Hindari penggunaan adverb yang menunjukkan masa lalu (seperti 'kemarin') dan masa depan (seperti 'besok')[/color].",
+			"malkuth": "1",
+			"content": "5"
+		},
+		"11" = {
+			"text": "Dalam kalimat, posisi adverb berada di [color=yellow]akhir kalimat sesudah object[/color].",
+			"malkuth": "2",
+			"content": "6"
+		},
+		"12" = {
+			"text": "Untuk menulis keterangan waktu, teman-teman bisa menggunakan ketiga kata penghubung di samping ini. Setiap kata hubung ini memiliki penggunaannya masing-masing seperti berikut ini.",
+			"malkuth": "1",
+			"content": "7"
+		},
+		"13" = {
+			"text": "Dalam simple present tense, 'In' dapat digunakan untuk menunjukkan [color=yellow]rentang waktu secara umum seperti hari, nama bulan, dan nama musim[/color]. Contoh: in the morning, in August, in summer.",
+			"malkuth": "1",
+			"content": "8"
+		},
+		"14" = {
+			"text": "Selanjutnya dalam simple present tense, 'At' dapat digunakan untuk menunjukkan [color=yellow]waktu secara lebih spesifik[/color] seperti jam atau saat tertentu. Contoh: at 7:30, at midday, at sunrise, at weekend, at Idul Fitri.",
+			"malkuth": "1",
+			"content": "9"
+		},
+		"15" = {
+			"text": "Ada satu pengecualian yaitu untuk keterangan waktu 'night' (malam hari), gunakan kata hubung 'at', misal 'I sleep [color=aqua]at night[/color]'.",
+			"malkuth": "2",
+			"content": "9"
+		},
+		"16" = {
+			"text": "Dan terakhir ada kata hubung waktu 'on'. Kata ini digunakan untuk menunjukkan [color=yellow]tanggal atau hari tertentu[/color]. Contoh: on 25 June, on Friday, on Mondays, on my birthday.",
+			"malkuth": "1",
+			"content": "10"
+		},
+		"17" = {
+			"text": "Penggunan kata hubung yang tepat penting teman-teman pelajari agar bisa menyusun kalimat dengan baik. Agar mudah mengingatnya, di sini ada perbedaan dari ketiga kata hubung yang telah kita pelajari.",
+			"malkuth": "3",
+			"content": "11"
+		},
+		"18" = {
+			"text": "Berikut ini beberapa contoh kalimat dengan adverb of time.",
+			"malkuth": "1",
+			"content": "12"
+		},
+		"19" = {
+			"text": "Mungkin di awal teman-teman akan tertukar antara penggunaan kata hubung satu dengan yang lainnya. Tetapi seiring teman-teman terbiasa membaca dan menulis Bahasa Inggris, teman-teman akan dapat menggunakan kata hubung yang sesuai dengan mudah.",
+			"malkuth": "2",
+			"content": "12"
+		},
+		"20" = {
+			"text": "Itu saja untuk pelajaran kita kali ini, sampai berjumpa di pelajaran selanjutnya!",
+			"malkuth": "3",
+			"content": "12"
+		}
+	}
+}
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
